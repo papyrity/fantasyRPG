1901dbcf85656a35dd0f4cfb143a04e563762c74
david <dnordinm@gmail.com>
2026-06-05 04:38:45 -0600

    added protected route so that users have to be logged in to view pages

--- fantasyFrontend/src/App.jsx
+++ fantasyFrontend/src/App.jsx
@@ -7,21 +7,26 @@ import QuestsPage from './pages/QuestsPage'
 import ProfilePage from './pages/ProfilePage'
 import SettingsPage from './pages/SettingsPage'
 import LoginPage from './pages/LoginPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 //sample login user: alice pass: hunter2
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={
+        <ProtectedRoute>
+        <Layout />
+        </ProtectedRoute>
+        }>
         <Route index element={<HomePage />} />
         <Route path="inventory" element={<InventoryPage />} />
         <Route path="craft" element={<CraftPage />} />
         <Route path="quests" element={<QuestsPage />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="settings" element={<SettingsPage />} />
-        <Route path="login" element={<LoginPage />} />
       </Route>
+      <Route path="login" element={<LoginPage />} />
     </Routes>
   )
 }
